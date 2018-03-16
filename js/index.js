45e1f055f985df4ac9262c2c1c8204081a6e6816
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-16 18:37:12 +0100

    make something that partially works

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-console.log("coucou hibou")
-
 import Poisson from "poisson-disk-sampling";
 
-const p = new Poisson([600, 300, 200], 20, 30, 10);
-const points = p.fill();
+export function createCanvas() {
+  const canvas = document.createElement("CANVAS");
+  canvas.width = 600;
+  canvas.height = 600;
+  canvas.style.backgroundColor = 'rgba(158, 167, 184, 0.2)';
+  document.body.appendChild(canvas);
+  setup(canvas);
+}
 
-console.log(points); //array of sample points, themselves represented as simple arrays
+function setup(canvas) {
+  let context = canvas.getContext('2d');
+  let centerX = canvas.width / 2;
+  let centerY = canvas.height / 2;
+  let radius = 1;
+
+  const p = new Poisson([200, 300], 5, 25, 30);
+  const points = p.fill();
+  points.forEach(([x,y]) => {
+    createDot(context, x, y, radius);
+  });
+}
+
+
+function createDot(context, x, y, radius) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'black';
+  context.fill();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  createCanvas();
+});
