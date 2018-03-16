193a1aac3e29b2fde33457c00f98a32beb7f8cd4
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-16 09:52:33 +0100

    use point js only for a test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 console.log("coucou hibou")
 
-var p = new Poisson([600, 300, 200], 20, 30, 10);
-var points = p.fill();
+import Poisson from "poisson-disk-sampling";
+
+const p = new Poisson([600, 300, 200], 20, 30, 10);
+const points = p.fill();
 
 console.log(points); //array of sample points, themselves represented as simple arrays
