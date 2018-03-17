39e984653308a7849b03d2b7c28f75a30948cd77
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-17 13:52:25 +0100

    add export image

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 import Poisson from "poisson-disk-sampling";
 
-export function createCanvas() {
+function createCanvas() {
   const canvas = document.createElement("CANVAS");
   canvas.width = 600;
   canvas.height = 800;
   canvas.id = 'myCanvas';
   canvas.style.backgroundColor = '#9E82B8';
   document.body.appendChild(canvas);
-  setup(canvas);
+  return canvas;
+}
+
+function createExportButtonImg(canvas) {
+  const button = document.createElement("BUTTON");
+  button.innerHTML = 'export to png';
+  button.onclick = () => { saveImage(canvas) };
+  document.body.appendChild(button);
 }
 
 function setup(canvas) {
@@ -22,7 +29,6 @@ function setup(canvas) {
 
 }
 
-
 function createDot(context, x, y, radius) {
   context.beginPath();
   context.arc(x, y, radius, 0, 2 * Math.PI, false);
@@ -30,6 +36,27 @@ function createDot(context, x, y, radius) {
   context.fill();
 }
 
+function saveImage(canvas) {
+  // create a second canvas
+  let destinationCanvas = document.createElement("canvas");
+  destinationCanvas.width = canvas.width;
+  destinationCanvas.height = canvas.height;
+
+  let destCtx = destinationCanvas.getContext('2d');
+  //create a rectangle with the desired color
+  destCtx.fillStyle = canvas.style.backgroundColor;
+  destCtx.fillRect( 0, 0, canvas.width, canvas.height);
+  
+  //draw the original canvas onto the destination canvas
+  destCtx.drawImage(canvas, 0, 0);
+  
+  const image = destinationCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+  window.location.href = image;
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
-  createCanvas();
+  const canvas = createCanvas();
+  createExportButtonImg(canvas);
+  setup(canvas);
 });
