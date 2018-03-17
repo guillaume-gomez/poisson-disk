baf3c1a087fb3e3418bc2c8698f74120d134c294
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-17 13:26:25 +0100

    add default color accorded to button

--- js/index.js
+++ js/index.js
@@ -3,24 +3,23 @@ import Poisson from "poisson-disk-sampling";
 export function createCanvas() {
   const canvas = document.createElement("CANVAS");
   canvas.width = 600;
-  canvas.height = 600;
+  canvas.height = 800;
   canvas.id = 'myCanvas';
-  canvas.style.backgroundColor = 'rgba(158, 167, 184, 0.2)';
+  canvas.style.backgroundColor = '#9E82B8';
   document.body.appendChild(canvas);
   setup(canvas);
 }
 
 function setup(canvas) {
   let context = canvas.getContext('2d');
-  let centerX = canvas.width / 2;
-  let centerY = canvas.height / 2;
-  let radius = 1;
 
+  let radius = 1;
   const p = new Poisson([200, 300], 5, 25, 30);
   const points = p.fill();
   points.forEach(([x,y]) => {
     createDot(context, x, y, radius);
   });
+
 }
 
 
